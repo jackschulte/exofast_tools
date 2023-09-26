2f82f0e419e7743978ea2ef8b19742780b7966e7
jackschulte <jschulte@msu.edu>
2023-09-26 15:00:01 -0400

    made a small change to remove a bug added when I tried to fix a warning

--- lightcurve_pipeline.py
+++ lightcurve_pipeline.py
@@ -422,7 +422,7 @@ def create_light_curve(target, author, sector, period=None, duration=None, tc=No
 
         #Plotting the light curve of only the transits
         fig2, ax2 = plt.subplots(figsize=(16, 8))
-        plt.scatter(time[tuple(in_transit)], flat_flux[tuple(in_transit)])
+        plt.scatter(time[in_transit], flat_flux[in_transit])
         plt.title("%s: Sector %s, %ss %s - Individual Transit Flux" % (targetname,sector,int(exposure),binflag))
         plt.xlabel("Time - 2457000 [BTJD days]")
         plt.ylabel("Normalized Flux")
@@ -444,7 +444,7 @@ def create_light_curve(target, author, sector, period=None, duration=None, tc=No
 
         #Plotting the phase folded light curve to easily observe transit
         fig4, ax4 = plt.subplots(figsize=(16, 8))
-        plt.scatter(phase[tuple(in_transit)], flat_flux[tuple(in_transit)])
+        plt.scatter(phase[in_transit], flat_flux[in_transit])
         plt.title("%s: Sector %s, %ss %s - Phase Folded Light Curve" % (targetname,sector,int(exposure),binflag)) 
         plt.xlabel("Phase")
         plt.ylabel("Normalized Fluxf")
@@ -468,7 +468,7 @@ def create_light_curve(target, author, sector, period=None, duration=None, tc=No
         np.savetxt('lc_output/n%s.TESS.TESS.%sFullFlat.S%s.%ss%s.dat' % (date,targetname, sector,int(exposure),binflag), \
                    np.c_[time, flat_flux, errors], delimiter=' ') 
         np.savetxt('lc_output/n%s.TESS.TESS.%sSlimFlat.S%s.%ss%s.dat' % (date,targetname, sector,int(exposure),binflag), \
-                   np.c_[time[tuple(in_transit)], flat_flux[tuple(in_transit)], errors[tuple(in_transit)]], delimiter=' ')    
+                   np.c_[time[in_transit], flat_flux[in_transit], errors[in_transit]], delimiter=' ')    
         
         # save individual transits to separate files
         for key, value in final_transits.items():
